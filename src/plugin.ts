1cf286774bf7478d2f694f4bde0e90445a7fc92f
Gordon Leigh <gordon@propulsionworks.io>
2024-10-05 21:21:29 +0200

    Chore: switch to zip24 for zip generation

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,10 @@
-import archiver from "archiver";
+import { buffer } from "node:stream/consumers";
 import type { Plugin } from "rollup";
+import { ZipWriter } from "zip24/writer";
 
+/**
+ * Options for the plugin.
+ */
 export type ZipPluginOptions = {
   /**
    * The name of the zip file.
@@ -14,56 +18,65 @@ export type ZipPluginOptions = {
   overrideModifiedDate?: Date;
 };
 
+/**
+ * A Rollup plugin to pack bundle files into a zip file.
+ *
+ * @see {@link https://github.com/gordonmleigh/rollup-plugin-zip#README}
+ */
 function zip(options: ZipPluginOptions = {}): Plugin {
   const { outputFileName = "bundle.zip", overrideModifiedDate } = options;
 
   return {
     name: "zip",
 
+    /**
+     * Called at the end of `bundle.generate()` or immediately before the files
+     * are written in `bundle.write()`. To modify the files after they have been
+     * written, use the `writeBundle` hook. bundle provides the full list of
+     * files being written or generated along with their details.
+     *
+     * You can prevent files from being emitted by deleting them from the
+     * `bundle` object in this hook. To emit additional files, use the
+     * `this.emitFile` plugin context function.
+     *
+     * @see {@link https://rollupjs.org/plugin-development/#generatebundle}
+     */
     async generateBundle(opts, bundle) {
-      const zip = archiver("zip", { zlib: { level: 9 } });
+      const zip = new ZipWriter();
       const names = Object.keys(bundle).sort((a, b) => a.localeCompare(b));
       const date = overrideModifiedDate ?? new Date();
 
-      let error: unknown;
-
-      zip.on("error", (err: unknown) => {
-        error = err || new Error(`unknown error occurred`);
-      });
-
       for (const name of names) {
         const chunkOrAsset = bundle[name];
+        // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
         delete bundle[name];
 
         if (chunkOrAsset.type === "asset") {
-          zip.append(Buffer.from(chunkOrAsset.source), {
-            name: chunkOrAsset.fileName,
-            date,
-          });
+          await zip.addFile(
+            {
+              path: chunkOrAsset.fileName,
+              lastModified: date,
+            },
+            chunkOrAsset.source,
+          );
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         } else if (chunkOrAsset.type === "chunk") {
-          zip.append(chunkOrAsset.code, {
-            name: chunkOrAsset.fileName,
-            date,
-          });
+          await zip.addFile(
+            {
+              path: chunkOrAsset.fileName,
+              lastModified: date,
+            },
+            chunkOrAsset.code,
+          );
         }
       }
 
-      // don't await this because it can hang forever
-      void zip.finalize();
-
-      const chunks: Buffer[] = [];
-      for await (const chunk of zip) {
-        chunks.push(chunk);
-      }
-
-      if (error) {
-        throw error;
-      }
+      await zip.finalize();
 
       this.emitFile({
         type: "asset",
         fileName: outputFileName,
-        source: Buffer.concat(chunks),
+        source: await buffer(zip),
       });
     },
   };
